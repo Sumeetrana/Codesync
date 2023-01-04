213ff82fa3da107e7913616a119f2132bda930ee
sumeet.rana <sumeetsinh28@gmail.com>
2023-01-04 23:02:43 +0530

    generating random roomId using uuid

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v4 as uuid } from 'uuid';
 
 const Home = () => {
+  const [roomId, setRoomId] = useState("");
+  const [username, setUsername] = useState("");
+
+  const createNewRoom = (e) => {
+    e.preventDefault();
+    const id = uuid();
+    setRoomId(id);
+  }
+
   return (
     <div className='homePageWrapper'>
       <div className='formWrapper'>
         <img className='homePageLogo' src="/code-sync.png" alt="code-sync logo" />
         <h4 className='mainLabel'>Paste invitation ROOM ID</h4>
         <div className='inputGroup'>
-          <input type="text" className='inputBox' placeholder='ROOM ID' />
-          <input type="text" className='inputBox' placeholder='USERNAME' />
+          <input
+            type="text"
+            value={roomId}
+            onChange={(e) => setRoomId(e.target.value)}
+            className='inputBox'
+            placeholder='ROOM ID'
+          />
+          <input
+            type="text"
+            value={username}
+            className='inputBox'
+            placeholder='USERNAME'
+            onChange={(e) => setUsername(e.target.value)}
+          />
           <button className='btn joinBtn'>Join</button>
           <span className='createInfo'>
             If you don't have an invite then create &nbsp;
-            <a href="" className='createNewBtn'>New room</a>
+            <a href="#" onClick={createNewRoom} className='createNewBtn'>New room</a>
           </span>
         </div>
       </div>
