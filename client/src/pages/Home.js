7aea198b02ac86ff0430cd3a8938ac538c063f7f
sumeet.rana <sumeetsinh28@gmail.com>
2023-01-04 22:57:13 +0530

    home page styling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,8 +2,23 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className='homePageWrapper'>
+      <div className='formWrapper'>
+        <img className='homePageLogo' src="/code-sync.png" alt="code-sync logo" />
+        <h4 className='mainLabel'>Paste invitation ROOM ID</h4>
+        <div className='inputGroup'>
+          <input type="text" className='inputBox' placeholder='ROOM ID' />
+          <input type="text" className='inputBox' placeholder='USERNAME' />
+          <button className='btn joinBtn'>Join</button>
+          <span className='createInfo'>
+            If you don't have an invite then create &nbsp;
+            <a href="" className='createNewBtn'>New room</a>
+          </span>
+        </div>
+      </div>
+      <footer>
+        <h4>Built with 💛 &nbsp;by <span>Sumeet Rana</span></h4>
+      </footer>
     </div>
   );
 };
