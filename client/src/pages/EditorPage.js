2963834e79e140186b9cb3a75c34ffdb6806c9d6
sumeet.rana <sumeetsinh28@gmail.com>
2023-01-04 23:50:43 +0530

    editor page guest list design

--- client/src/pages/EditorPage.js
+++ client/src/pages/EditorPage.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Client from '../components/Client';
+import Editor from '../components/Editor';
 
 const EditorPage = () => {
+  const [clients, setClients] = useState([
+    {
+      socketId: 1,
+      username: 'Harry P'
+    },
+    {
+      socketId: 2,
+      username: 'Emma W'
+    },
+    {
+      socketId: 3,
+      username: 'Ron W'
+    },
+  ])
   return (
-    <div>
-      Editor Page
+    <div className='mainWrap'>
+      <div className='aside'>
+        <div className='asideInner'>
+          <div className='logo'>
+            <img src="/code-sync.png" alt="logo" className='logo-image' />
+          </div>
+          <h3>Connected</h3>
+          <div className='clientsList'>
+            {clients.map((client) => {
+              return <Client username={client.username} key={client.socketId} />
+            })}
+          </div>
+        </div>
+        <button className='btn copyBtn'>Copy ROOM ID</button>
+        <button className='btn leaveBtn'>Leave</button>
+      </div>
+      <div className="editorWrap">
+        <Editor />
+      </div>
     </div>
   );
 };
